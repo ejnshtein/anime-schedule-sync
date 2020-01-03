b66b230349d4f5faaee6636aa7204cc1c8956830
ejnshtein <ejnshtein@gmail.com>
2020-01-02 19:55:32 -0800

    v1.0.2

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 
-const fs = require('fs')
-const readline = require('readline')
-const { google } = require('googleapis')
-const colors = require('./color.json')
+import { promises as fs, readFileSync } from 'fs'
+import { createInterface } from 'readline'
+import googleapis from 'googleapis'
+import { parseTime, parseDate, sleep, argv, getArgv } from './lib/index.js'
+const { google } = googleapis
+const colors = JSON.parse(readFileSync('./color.json'))
 
-const rl = readline.createInterface({
+const config = {}
+
+try {
+  const { spreadsheetId, range, calendarId } = JSON.parse(readFileSync(getArgv('--config') || './config.json'))
+  config.spreadsheetId = spreadsheetId || null
+  config.range = range || null
+  config.calendarId = calendarId || null
+} catch (e) {}
+
+for (const key in config) {
+  console.log(`${key} loaded from ${getArgv('--config') || './config.json'}`)
+}
+
+const { readFile, writeFile } = fs
+
+const rl = createInterface({
   input: process.stdin,
   output: process.stdout
 })
@@ -19,85 +36,108 @@ const SCOPES = [
 // time.
 const TOKEN_PATH = 'token.json'
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err)
-  // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), start)
-  // authorize(JSON.parse(content), listEvents)
-})
+const formatDate = data => data.toString().length < 2 ? `0${data}` : data
 
+const proceed = async () => {
+  const content = JSON.parse(await readFile('client_secret.json'))
+  const oAuth2Client = await authorize(content)
+
+  return start(oAuth2Client)
+}
+proceed()
+  .then(() => {
+    process.exit(1)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
  * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
  */
-function authorize (credentials, callback) {
+async function authorize (credentials) {
   const {
     client_secret,
     client_id,
     redirect_uris
   } = credentials.installed
   const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0])
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  )
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback)
+  try {
+    const token = await readFile(TOKEN_PATH)
     oAuth2Client.setCredentials(JSON.parse(token))
-    callback(oAuth2Client)
-      .then(() => {
-        console.log('Done')
-      })
-      .catch(err => {
-        console.log(`Error: ${err.message}`)
-        console.log(err.stack)
-      })
-  })
+    return oAuth2Client
+  } catch (e) {
+    return getNewToken(oAuth2Client)
+  }
 }
 
 /**
  * Get and store new token after prompting for user authorization, and then
  * execute the given callback with the authorized OAuth2 client.
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getNewToken (oAuth2Client, callback) {
+async function getNewToken (oAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
   })
   console.log('Authorize this app by visiting this url:', authUrl)
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close()
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err)
-      oAuth2Client.setCredentials(token)
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err)
-        console.log('Token stored to', TOKEN_PATH)
+  return getAnswer('Enter the code from that page here: ')
+    .then(code => {
+      return new Promise((resolve, reject) => {
+        oAuth2Client.getToken(code, (err, token) => {
+          if (err) return reject(new Error(`Error while trying to retrieve access token ${err}`))
+          oAuth2Client.setCredentials(token)
+          // Store the token to disk for later program executions
+          writeFile(TOKEN_PATH, JSON.stringify(token))
+            .then(() => {
+              console.log('Token stored to', TOKEN_PATH)
+              resolve(oAuth2Client)
+            })
+            .catch(reject)
+        })
       })
-      callback(oAuth2Client)
     })
-  })
+  // rl.question('Enter the code from that page here: ', (code) => {
+  //   rl.close()
+  //   oAuth2Client.getToken(code, (err, token) => {
+  //     if (err) return console.error('Error while trying to retrieve access token', err)
+  //     oAuth2Client.setCredentials(token)
+  //     // Store the token to disk for later program executions
+  //     writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+  //       if (err) console.error(err)
+  //       console.log('Token stored to', TOKEN_PATH)
+  //     })
+  //     callback(oAuth2Client)
+  //   })
+  // })
 }
 
 async function start (auth) {
-  const spreadsheetId = await AnimeTransferApp.getSpreadsheetId()
-  const range = await AnimeTransferApp.getRange()
-  const calendarId = await AnimeTransferApp.getCalendarId()
+  const spreadsheetId = config.spreadsheetId || await AnimeTransferApp.getSpreadsheetId()
+  const range = config.range || await AnimeTransferApp.getRange()
+  const calendarId = config.calendarId || await AnimeTransferApp.getCalendarId()
   const app = new AnimeTransferApp({ auth, calendarId, spreadsheetId, range })
   const animes = await app.getAnimes()
-  const go = await getAnswer(`There's ${animes.length} animes.\n\nContinue? (enter any value and press enter to terminate)`, false)
+
+  const go = await getAnswer(
+    `There's ${animes.length} animes.
+  Continue? (enter any value and press enter to terminate)`,
+    false
+  )
   if (go) {
     console.log('Terminating...')
   }
   const result = await app.addAnimesToCalendar(animes)
   console.log('Done')
   console.log(result)
-  process.exit(1)
+  return result
 }
 
 class AnimeTransferApp {
@@ -114,10 +154,7 @@ class AnimeTransferApp {
       process.exit(0)
     }
     this.spreadsheetId = spreadsheetId
-    if (!range) {
-      console.log('no range was given, try again')
-      process.exit(0)
-    }
+
     this.range = range
 
     if (!calendarId) {
@@ -144,12 +181,12 @@ class AnimeTransferApp {
     if (argv) {
       var argvSheetId = getArgv('--sheetid')
     }
-    const spreadsheetId = !argvSheetId ? await getAnswer('Gimme spreadsheet id: ') : argvSheetId
+    const spreadsheetId = argvSheetId || await getAnswer('Gimme spreadsheet id: ')
     if (!spreadsheetId) {
-      console.log(`no id was given, try again`)
+      console.log('no id was given, try again')
       return AnimeTransferApp.getSpreadsheetId(false)
     }
-    console.log(`Got sheet id${argv ? ' from command line' : ''}: ${spreadsheetId}`)
+    console.log(`Got sheet id${argv && argvSheetId ? ' from command line' : ''}: ${spreadsheetId}}`)
     return spreadsheetId
   }
 
@@ -157,12 +194,8 @@ class AnimeTransferApp {
     if (argv) {
       var argvRangeId = getArgv('--range')
     }
-    const range = !argvRangeId ? await getAnswer('Gimme range for this spreadsheet values (default - A2:H9): ', 'A2:H9') : argvRangeId
-    if (!range) {
-      console.log(`no id was given`)
-      return AnimeTransferApp.getRange(false)
-    }
-    console.log(`Got range${argv ? ' from command line' : ''}: ${range}`)
+    const range = argvRangeId || await getAnswer('Gimme range for this spreadsheet values (default - A2:H9)(If you want to specify sheet then: "Sheet name"!A2:K9): ', 'A2:H9')
+    console.log(`Got range${argv && argvRangeId ? ' from command line' : ''}: ${range}`)
     return range
   }
 
@@ -170,29 +203,22 @@ class AnimeTransferApp {
     if (argv) {
       var argvCalendarId = getArgv('--calendarid')
     }
-    const calendarId = !argvCalendarId ? await getAnswer('Gimme calendar id: ') : argvCalendarId
+    const calendarId = argvCalendarId || await getAnswer('Gimme calendar id: ')
     if (!calendarId) {
       console.log('no id was given, try again')
       return AnimeTransferApp.getCalendarId(false)
     }
-    console.log(`Got calendar id${argv ? ' from command line' : ''}: ${calendarId}`)
+    console.log(`Got calendar id${argv && argvCalendarId ? ' from command line' : ''}: ${calendarId}`)
     return calendarId
   }
 
   async getAnimes () {
-    console.log(this.spreadsheetId, this.range)
     const sheetsValues = await this.sheets.spreadsheets.values.get({
       spreadsheetId: this.spreadsheetId,
       range: this.range
     })
     return flatten(
-      sheetsValues.data.values.map(el =>
-        el
-          .filter(Boolean)
-          .filter(el => /^(.+?)\s?(?::|：)? ([0-9]{2}\/[0-9]{2}) ([0-9]{2}:[0-9]{2}) (\d+) (.+)$/i.test(el)
-          )
-          .map(parseAnimeData)
-      ),
+      sheetsValues.data.values.map(el => el.filter(Boolean).map(parseAnimeData)),
       1
     )
   }
@@ -206,7 +232,7 @@ class AnimeTransferApp {
         time.hour = time.hour - 24
         date.date++
       }
-      let startDateTime = `2019-${formatDate(date.month)}-${formatDate(date.date)}T${formatDate(time.hour)}:${formatDate(time.minute)}:00`
+      const startDateTime = `2020-${formatDate(date.month)}-${formatDate(date.date)}T${formatDate(time.hour)}:${formatDate(time.minute)}:00`
       if (time.minute >= 30) {
         time.hour++
         time.minute = time.minute - 30
@@ -217,7 +243,7 @@ class AnimeTransferApp {
         time.hour = time.hour - 24
         date.date += 1
       }
-      let endDateTime = `2019-${formatDate(date.month)}-${formatDate(date.date)}T${formatDate(time.hour)}:${formatDate(time.minute)}:00`
+      const endDateTime = `2020-${formatDate(date.month)}-${formatDate(date.date)}T${formatDate(time.hour)}:${formatDate(time.minute)}:00`
       const link = `https://myanimelist.net/anime/${anime.id}`
       try {
         var res = await this.calendar.events.insert({
@@ -242,7 +268,7 @@ class AnimeTransferApp {
       }
       await sleep(1500)
       response.push({
-        calendarResult: res
+        calendarResult: res.data
       })
     }
     return response
@@ -261,7 +287,10 @@ function getAnswer (question, defaultAnswer) {
 }
 
 function parseAnimeData (anime) {
-  const animeData = anime.match(/^(.+?)\s?(?::|：)? ([0-9]{2}\/[0-9]{2}) ([0-9]{2}:[0-9]{2}) (\d+) (.+)$/i)
+  const animeData = anime.match(/^(.+?)\s?(?::|：)?\s([0-9]{2}\/[0-9]{2})\s([0-9]{2}:[0-9]{2})\s(\d+)\s(.+)$/im)
+  if (argv('--debug')) {
+    console.log('parseAnimeData ', anime, animeData)
+  }
   return {
     id: Number(animeData[4]),
     channel: animeData[1],
@@ -278,34 +307,6 @@ function findColor (channel) {
   try {
     return colors[Object.keys(colors).map(el => ({ key: el, lowerCase: el.toString().toLowerCase() })).find(el => el.lowerCase === channel.toLowerCase()).key]
   } catch (e) {
-    return colors['other']
+    return colors.other
   }
-}
-
-const { parseInt } = Number
-
-const parseTime = time => {
-  const parsed = time.match(/([0-9]+):([0-9]+)/i)
-  return {
-    hour: parseInt(parsed[1]),
-    minute: parseInt(parsed[2])
-  }
-}
-const parseDate = date => {
-  const parsed = date.match(/([0-9]+)\/([0-9]+)/i)
-  return {
-    date: parseInt(parsed[2]),
-    month: parseInt(parsed[1])
-  }
-}
-const formatDate = data => data.toString().length < 2 ? `0${data}` : data
-
-const sleep = timeout => new Promise(resolve => setTimeout(resolve, timeout))
-
-function getArgv (name) {
-  const id = process.argv.indexOf(name)
-  if (id !== -1) {
-    return process.argv[id + 1]
-  }
-  return undefined
 }
